23771c768c450f53689480280ccf1b3fc35ddd59
sun <sleepsun123@163.com>
2020-05-25 00:53:51 +0800

    fix url have param

--- getdata/getdata.go
+++ getdata/getdata.go
@@ -2,6 +2,7 @@ package getdata
 
 import (
 	"encoding/base64"
+	"encoding/json"
 	"log"
 	"net/http"
 	"regexp"
@@ -10,6 +11,20 @@ import (
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type Vary struct {
+	Version string `json:"v"`
+	Host    string `json:"host"`
+	Path    string `json:"path"`
+	TLS     string `json:"tls"`
+	Ps      string `json:"ps"`
+	Add     string `json:"add"` // url
+	Prot    string `json:"port"`
+	ID      string `json:"id"`
+	Aid     string `json:"aid"`
+	Net     string `json:"net"`
+	Type    string `josn:"type"`
+}
 
 // ExampleScrape get telegarm v2list page data
 func ExampleScrape(count string, cors bool) (string, bool) {
@@ -48,24 +63,93 @@ func ExampleScrape(count string, cors bool) (string, bool) {
 func MakeList(d string) []string {
 	x := []string{}
 	l := strings.Split(d, "vmess://")
-	for _, item := range l {
+	for i, item := range l {
 		var l int
 		l = len(item)
 		if l > 0 {
 			var strHaiCoder string
 			var newstr string
 			var v string
+			var other bool
 
-			decodeBytes, err := base64.StdEncoding.DecodeString(item)
-			if err != nil {
-				return x
+			other = strings.Contains(item, "?remarks=")
+			if other {
+				strsss := strings.Split(item, "?remarks=")
+				var strtobyte []byte = []byte(strsss[0])
+				decodeBytes := make([]byte, base64.StdEncoding.DecodedLen(len(strtobyte))) // 计算解码后的长度
+				base64.StdEncoding.Decode(decodeBytes, strtobyte)
+
+				newstr := string(decodeBytes[:])
+				// fmt.Println(newstr)
+				blen := len(newstr)
+				a := strings.Index(newstr, ":")
+				b := strings.Index(newstr, "@")
+				c := strings.LastIndex(newstr, ":")
+				uuid := newstr[a+1 : b]
+				host := newstr[b+1 : c]
+				port := newstr[c+1 : blen]
+
+				if port == "4" || port == "44" {
+					port = "443"
+				}
+
+				params := strsss[1]
+				plen := len(params)
+				e := strings.Index(params, "path=")
+				f := strings.Index(params, "obfs=")
+				g := strings.Index(params, "tls=")
+				path := params[e+5 : f-1]
+				obfs := params[f+5 : g-1]
+				tls := params[g+4 : plen]
+
+				if obfs == "websocket" {
+					obfs = "ws"
+				} else {
+					obfs = "tcp"
+				}
+
+				if tls == "1" {
+					tls = "tls"
+				} else {
+					tls = "tcp"
+				}
+
+				// fmt.Println(uuid, host, port, param, path, obfs, tls)
+				// log.Println(uuid, host, port, param, path, obfs, tls)
+				cumv := strconv.Itoa(i)
+				myname := strings.Join([]string{"我的节点", cumv}, "-")
+				vjson := &Vary{
+					Version: "2",
+					Host:    host,
+					Path:    path,
+					TLS:     tls,
+					Ps:      myname,
+					Add:     host,
+					Prot:    port,
+					ID:      uuid,
+					Aid:     "1",
+					Net:     obfs,
+					Type:    "null",
+				}
+				bytes, err := json.Marshal(vjson)
+				if err != nil {
+					return x
+				}
+				// fmt.Println(string(bytes))
+				v = strings.Join([]string{"vmess:", base64.StdEncoding.EncodeToString(bytes)}, "//")
+				x = append(x, v)
+			} else {
+				decodeBytes, err := base64.StdEncoding.DecodeString(item)
+				if err != nil {
+					return x
+				}
+				strHaiCoder = `"ps" :"翻墙党fanqiangdang.com","" :`
+				reg := regexp.MustCompile(strHaiCoder)
+				newstr = reg.ReplaceAllString(string(decodeBytes), `"ps" :`)
+				var strtobyte []byte = []byte(newstr)
+				v = strings.Join([]string{"vmess:", base64.StdEncoding.EncodeToString(strtobyte)}, "//")
+				x = append(x, v)
 			}
-			strHaiCoder = `"ps" :"翻墙党fanqiangdang.com","" :`
-			reg := regexp.MustCompile(strHaiCoder)
-			newstr = reg.ReplaceAllString(string(decodeBytes), `"ps" :`)
-			var strtobyte []byte = []byte(newstr)
-			v = strings.Join([]string{"vmess:", base64.StdEncoding.EncodeToString(strtobyte)}, "//")
-			x = append(x, v)
 		}
 	}
 	return x
